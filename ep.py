f7ef6079a55a727e785d2ea516643be28188e39d
Renan Tiago <renan.tssilva@gmail.com>
2017-05-20 16:05:49 -0300

    adiciona metodos de euler e euler cromer

--- ep.py
+++ ep.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
+
 #lê o o numero do arquivo de de tempos do mruv
+
 def timestamps(filenumber):
     times = []
     first_line = True
     for i in range(1,5):
-        for row in csv.reader(open(experimento + str(filenumber) + '.csv', 'rt')):
+        for row in csv.reader(open("mruv/experimento" + str(filenumber) + '.csv', 'rt')):
             if first_line == False:
                 t1 = float(row[0])
                 t2 = float(row[1])
@@ -36,6 +38,58 @@ def accel_run(times):
     accel = (vf-vo)/tf_to
 
     return accel
+
+def eulerPosition(y0, v0, dt):
+    return y0 + v0 * dt
+
+#retorna v0 + g * seno_da_inclinação_da_rampa
+#def eulerVelocity(v0, dt):
+#    return v0 + 9.8 * 0.0784591 * dt
+def eulerVelocity(v0, acc, dt):
+    if v0 == 0:
+        return v0 + 9.8 * 0.0784591 * dt
+    else:
+        return v0 + (9.8 * 0.0784591 - 0.0295/v0) * dt
+
+
+def euler(acc, dt):
+    t0 = 0
+    y = []
+    v = []
+    t = []
+    y.append(0)
+    v.append(0)
+    t.append(t0)
+    i = 0
+
+    while(y[i] < 30):
+        t0 += dt
+        y.append(eulerPosition(y[i], v[i], dt))
+        v.append(eulerVelocity(v[i], acc, dt))
+        t.append(t0)
+        i += 1
+
+    return y, v, t
+
+def eulerCromer (acc, dt):
+    t0 = 0
+    i = 0
+    v = []
+    y = []
+    t = []
+    y.append(0)
+    v.append(0)
+    t.append(t0)
+
+    while(y[i] < 30):
+        t0 += dt
+        v.append(eulerVelocity(v[i], acc, dt))
+        y.append(eulerPosition(y[i], v[i + 1], dt))
+        t.append(t0)
+        i += 1
+
+    return y, v, t
+
 
 def pendulum_times(filenametoolbox):
     # Função que recebe um arquivo "filenametoolbox" contendo dados do acelerômetro
@@ -72,7 +126,12 @@ def pendulum_times(filenametoolbox):
 
     return times
 
-times = timestamps("mruv/experimento5")
+space = [10,20,30]
+times = timestamps(5)
 acc = accel_run(times)
-print(times)
-print(acc)
+y,v,t = euler(acc, 0.05)
+y1,v1,t1 = eulerCromer(acc, 0.05)
+plt.plot(times, space, "bo", t, y, "r", t1, y1, 'g')
+plt.show()
+#print(times)
+#print(acc)
